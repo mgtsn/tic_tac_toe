8218438f4f9986c049ddfda2d7a029b0e3144c17
Morgan <morgantosini@gmail.com>
2020-08-30 13:18:50 -0400

    started mastermind

---
+++ mastermind.rb
@@ -0,0 +1,38 @@
+class Mastermind
+  @@COLORS = [0, 1, 2, 3, 4, 5]
+  @@MAX_GUESSES = 8
+
+  def initialize(code)
+    @code = code
+  end
+
+  def check_guess(guess)
+    @code == guess
+  end
+
+  def get_input
+    guess = []
+    4.times do |n|
+      puts "Guess color number #{n + 1}"
+      input = gets.chomp.to_i
+      guess.push(input)
+    end
+    guess
+  end
+
+  def play
+    guess_count = 1
+    finished = false
+    until finished or guess_count > @@MAX_GUESSES
+      puts "Guess number #{guess_count}"
+      input = get_input
+      finished = check_guess(input)
+      guess_count += 1
+    end
+    puts "done"
+  end
+end
+
+m = Mastermind.new([1, 2, 3, 4])
+
+m.play
